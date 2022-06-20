5ad5f9f70ce265c78e9b63d7b57e619636011a5d
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 17:17:54 +0530

    [Vishwas]add: Finding Maximum Float Program Added

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -7,7 +7,8 @@ namespace GenericsProgram
         {
             Console.WriteLine("Welcome to the Generics Program....");
             FindMaximum findingmax = new FindMaximum();
-            findingmax.MaximumInteger(45,98,78);
+            //findingmax.MaximumInteger(45,98,78);
+            findingmax.MaximumFloat(46.66, 63.99, 146.22);
         }
     }
 }
