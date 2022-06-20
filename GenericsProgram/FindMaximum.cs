5131babdca13e1443a29ddf093b916cbfef33330
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 17:13:36 +0530

    [Vishwas]add: Finding Maximum Integer Program Added

---
+++ GenericsProgram/FindMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    public class FindMaximum
+    {
+        public void MaximumInteger(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine("First Value {0} is greatest of all ", firstValue);
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine("Second Value {0} is greatest of all ", secondValue);
+            }
+            else
+            {
+                Console.WriteLine("Third Value {0} is greatest of all ", thirdValue);
+            }
+
+
+
+
+
+        }
+    }
+}
--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -6,6 +6,8 @@ namespace GenericsProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generics Program....");
+            FindMaximum findingmax = new FindMaximum();
+            findingmax.MaximumInteger(45,98,78);
         }
     }
 }
