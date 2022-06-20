36829c282a501063c2556253f4c891116e939f95
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 21:53:19 +0530

    [Vishwas]add: Finding Maximum Value By Generic Class

---
+++ GenericsProgram/MaxByGenerics.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    public class MaxByGenerics<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+
+        public MaxByGenerics(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaximumValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine("First Value {0} is greatest of all ", firstValue);
+                return firstValue;
+
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine("Second Value {0} is greatest of all ", secondValue);
+                return secondValue;
+            }
+            else
+            {
+                Console.WriteLine("Third Value {0} is greatest of all ", thirdValue);
+                return thirdValue;
+            }
+
+        }
+        public T MaxMehod()
+        {
+            T Max = MaxByGenerics<T>.MaximumValue(this.firstValue, this.secondValue, this.thirdValue);
+            return Max;
+        }
+
+    }
+    
+}
--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -6,15 +6,17 @@ namespace GenericsProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generics Program....");
-            FindMaximum<int> findingmaxint = new FindMaximum<int>();
-            findingmaxint.MaximumValue(46, 98, 77);
-            FindMaximum<double> findingmaxfloat = new FindMaximum<double>();
-            findingmaxfloat.MaximumValue(12.32, 36.33, 41.1);
-            FindMaximum<string> findingmaxstring = new FindMaximum<string>();
-            findingmaxstring.MaximumValue("15", "19", "26");
-            //findingmax.MaximumInteger(45,98,78);
-            //findingmax.MaximumFloat(46.66, 63.99, 146.22);
-            //findingmax.MaximumString("45", "66", "78");
+            MaxByGenerics<int> findingmaxint = new MaxByGenerics<int>(111,22,33);
+            findingmaxint.MaxMehod();
+            MaxByGenerics<double> findingmaxfloat = new MaxByGenerics<double>(11.43, 43.11, 34.22);
+            findingmaxfloat.MaxMehod();
+            MaxByGenerics<string> findingmaxstring = new MaxByGenerics<string>("55", "34", "78");
+            findingmaxstring.MaxMehod();
+            Console.WriteLine("----------------------------------");
+            FindMaximum maxint = new FindMaximum();
+            maxint.MaximumInteger(45,98,78);
+            maxint.MaximumFloat(46.66, 63.99, 146.22);
+            maxint.MaximumString("45", "66", "78");
         }
     }
 }
