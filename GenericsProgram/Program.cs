570aebe7f762c5d34713f82c444c3c46e5623667
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 17:03:28 +0530

    [Vishwas]add: Generics Program Added

---
+++ GenericsProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GenericsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics Program....");
+        }
+    }
+}
